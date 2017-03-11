6e6c2e2599a16fd7a811e784c3b4d370384306a6
platisd <kurukii88@gmail.com>
2017-03-11 16:45:48 +0100

    Time left calculated for the next two trips of every bus

--- departures.py
+++ departures.py
@@ -6,13 +6,16 @@ from socket import AF_INET, SOCK_DGRAM
 import sys
 import socket
 import struct, time
+from datetime import datetime
+from collections import defaultdict
 
 apiKey = None
 apiSecret = None
+pathToConfig = "api-config"
 
 LINDHOLMEN_ID = "9021014004490000"
 TEKNIKGATAN_ID = "9021014006675000"
-stations = [LINDHOLMEN_ID, TEKNIKGATAN_ID]
+stations = {"Lindholmen": LINDHOLMEN_ID, "Teknikgatan": TEKNIKGATAN_ID}
 
 
 # Fetches the time from NTP server. Source: http://blog.mattcrampton.com/post/88291892461/query-an-ntp-server-from-python
@@ -32,8 +35,6 @@ def getNTPTime(host = "pool.ntp.org"):
 
     t = struct.unpack( "!12I", msg )[10]
     t -= TIME1970
-    #return time.ctime(t).replace("  "," ")
-    #return time.strftime("%H:%M", time.ctime(t))
     d = time.strptime(time.ctime(t),"%a %b %d %H:%M:%S %Y")
     return (time.strftime("%Y-%m-%d", d), time.strftime("%H:%M", d))
 
@@ -41,7 +42,6 @@ def getNTPTime(host = "pool.ntp.org"):
 # Fethes the consumer and secret key from configuration file (not version controlled).
 # The first line is the key and the second is the secret.
 def initAPIkeys():
-    pathToConfig = "api-config"
     f = open(pathToConfig)
     global apiKey, apiSecret
     apiKey = f.readline().replace('\n', "")
@@ -55,14 +55,35 @@ def main():
     # Get the current time and date from an NTP server as the host might not have an RTC
     (currentDate, currentTime) = getNTPTime()
     # Initialize the connection to the Vasttrafik server
-    vasttrafik = pytrafik.client.Client("json", apiKey, apiSecret)
-    for station in stations:
+    try:
+        vasttrafik = pytrafik.client.Client("json", apiKey, apiSecret)
+    except Exception as e:
+        print ("Authentication failure, exiting!")
+    trips = defaultdict(list)  # A dictionary of lists, holding a list of departures for each bus
+    for stationName, stationID in stations.items():
         # Get the departures for each station we are interested in
-        departures = vasttrafik.get_departures(station, date=currentDate, time=currentTime)
+        try:
+            departures = vasttrafik.get_departures(stationID, date=currentDate, time=currentTime)
+        except Exception as e:
+            print ("Connection failure for station %s" % stationName)
+            departures = []  # If something went wrong, empty the departures list so we don't try to iterate through them
         for departure in departures:
-            #print (departure['direction'])
-            if departure['track'] < 'C':
-                print ("Bus %s leaves towards %s at %s" % (departure['sname'], departure['direction'], departure['time']))
+            if departure['track'] < 'C':  # We only care for buses going towards the center for now
+                try:
+                    trips[departure['sname']].append(departure['rtTime'])
+                    #print ("Bus %s leaves towards %s at %s" % (departure['sname'], departure['direction'], departure['rtTime']))
+                except Exception as e:
+                    print ("Error while parsing server response")
+    #print (sorted(trips.items()))
+    for busLine, departureTimes in trips.items():
+            remainingDepartures = 2  # The number of departures that we care to show
+            for departureTime in departureTimes:
+                remainingDepartures -= 1
+                if remainingDepartures < 0:
+                    break
+                minutesToLeave = ((datetime.strptime(departureTime, "%H:%M") - datetime.strptime(currentTime, "%H:%M")).total_seconds() / 60)
+                print ("Bus %s, leaves in %d" % (busLine, minutesToLeave))
+
 
 
 if __name__ == "__main__":
